6f9d1c62a755b8ad2572d430195ae4be4b003f8f
Lowkeymvyk0l <162487587+Lowkeymvyk0l@users.noreply.github.com>
2024-03-09 21:58:54 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,7 @@
-print("alo")
-print("alo2")
+#pregunta 1: los cuatro pilares fundamentales de la poo es abstracción, encapsulamiento, herencia y polimorfismo.
+#Cuando hacemos una abstracción, queremos omitir detalles que no son necesarios para nosotros
+#El encapsulamiento nos permite controlar quién puede ver y utilizar los distintos módulos internos de nuestro sistema
+#La herencia es una relación especial entre dos clases, la clase base y la clase derivada
+#
 print("hola xavier3")
 #Xavier Soliz
