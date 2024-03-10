a4155184b4d7cfe04144c87016eb905d34089b8d
Lowkeymvyk0l <162487587+Lowkeymvyk0l@users.noreply.github.com>
2024-03-10 18:46:41 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,12 @@
 #pregunta 1: los cuatro pilares fundamentales de la poo es abstracción, encapsulamiento, herencia y polimorfismo.
-#Cuando hacemos una abstracción, queremos omitir detalles que no son necesarios para nosotros#El encapsulamiento nos permite controlar quién puede ver y utilizar los distintos módulos internos de nuestro sistema#La herencia es una relación especial entre dos clases, la clase base y la clase derivada
-
-
-#
+#Cuando hacemos una abstracción, queremos omitir detalles que no son necesarios para nosotros
+#El encapsulamiento nos permite controlar quién puede ver y utilizar los distintos módulos internos de nuestro sistema
+#La herencia es una relación especial entre dos clases, la clase base y la clase derivada
+#el polimorfismo es la capacidad de un aspecto de diferentes clases a responder a un mismo mensaje
+#pregunta 2 - la clase en python se refiere a una estructura que actua como plantilla para la creacion a los objetos es tas pueden contener atributos y metodos
+#pregunta 3 - un objeto es una entidad que contiene atributos que serias las caracteristicas de los objetos y tambien contiene metodos que seria el compartamiento del objeto
+#michael rafael
 #Xavier Soliz
 
 if __name__ == "__main__":
-    print(f"ssa{2}" + "\n" + "se")
+    print(f"ssa{2}" + "\n" + "se
