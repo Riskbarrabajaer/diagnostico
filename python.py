fcfc7c64bd7dabdd97e016bd63d1f27e5754e405
Lowkeymvyk0l <162487587+Lowkeymvyk0l@users.noreply.github.com>
2024-03-10 12:55:47 -0300

    Update python.py

--- python.py
+++ python.py
@@ -2,6 +2,6 @@
 #Cuando hacemos una abstracción, queremos omitir detalles que no son necesarios para nosotros
 #El encapsulamiento nos permite controlar quién puede ver y utilizar los distintos módulos internos de nuestro sistema
 #La herencia es una relación especial entre dos clases, la clase base y la clase derivada
-#
+#fbvrwgwrgvrwvrwv
 print("hola xavier3")
 #Xavier Soliz
